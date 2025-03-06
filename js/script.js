a592b10c633e60e56fb34dbccf25a9d7e33b9b21
EstebanCaponio <estebancaponio@gmail.com>
2025-03-06 15:13:26 +0100

    feat. created countdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('ciao');
+// creazione delle variabili necessarie
+const countdown = document.getElementById('countdown');
+
+countdown.innerText = '10';
+
+const Count30 = setInterval(CountdownNumb, 1000);
+
+let numb = 10;
+
+function CountdownNumb() {
+    console.log(numb);
+        numb--;
+        countdown.innerText = numb;
+
+    if(numb===0){
+        clearInterval(Count30);
+    }
+}
