4ef563d78dacde79fbacde9e4bfd58a54426b74b
EstebanCaponio <estebancaponio@gmail.com>
2025-03-06 16:32:21 +0100

    feat: created 5 different random numbers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // creazione delle variabili necessarie
 const countdown = document.getElementById('countdown');
+const randomList = document.getElementById('numbers-list');
 
+// programmaz. countdown
 countdown.innerText = '10';
 
 const Count30 = setInterval(CountdownNumb, 1000);
@@ -9,10 +11,42 @@ let numb = 10;
 
 function CountdownNumb() {
     console.log(numb);
-        numb--;
-        countdown.innerText = numb;
+    numb--;
+    countdown.innerText = numb;
 
-    if(numb===0){
+    if (numb === 0) {
         clearInterval(Count30);
+        randomList.classList.add("d-none");
     }
+}
+
+// creo array che mi serve per confrontare i numeri che non devono essere uguali
+const randomNumbers = [];
+
+// generazione numeri random
+for (i = 0; i < 5; i++) {
+    // creo <li>
+    const liNumb = document.createElement('li');
+
+    let number
+    do {
+        number = Random50();
+    } while (randomNumbers.includes(number));
+
+    randomNumbers.push(number);
+
+    liNumb.innerText = randomNumbers[i];
+    randomList.append(liNumb);
+
+}
+console.log(randomNumbers);
+
+
+
+
+
+
+// funzione nmeri random 1-50
+function Random50() {
+    return Math.floor(Math.random() * 50) + 1;
 }
